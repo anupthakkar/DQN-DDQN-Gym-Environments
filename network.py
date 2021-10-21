85adeb0e7de3fd6e8af6288fa15358d1781992ef
pushkaraj <pushkaraj803@gmail.com>
2021-10-21 01:24:08 -0400

    Testing comment

--- network.py
+++ network.py
@@ -21,7 +21,7 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
-
+        # comment 1
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
