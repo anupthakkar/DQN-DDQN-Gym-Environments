6148bcc16fbaf4be73d60de940671f5c268b9ccc
Anup Thakkar <anupthakker7@gmail.com>
2021-10-31 00:08:36 -0400

    updated DQN gpu

--- dqn_gpu.py
+++ dqn_gpu.py
@@ -10,7 +10,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-plt.rcParams["figure.figsize"] = (20,15)
+plt.rcParams["figure.figsize"] = (10,10)
 # from environment import GridEnvironment
 from network import Net
 from replay_memory import ReplayMemory
@@ -21,8 +21,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 class Main:
     def __init__(self, env_variable, env_type, input_size, output_size):
         # Here we are setting the parameters for the training and the neural networks.
-        self.training_episodes = 1000
-        self.epsilon_decay = 0.01
+        self.training_episodes = 10000
+        self.epsilon_decay = 0.001
         self.epsilon = 1
         self.discount_factor = 0.95
         self.env = env_variable
@@ -30,8 +30,8 @@ class Main:
         self.learning_target = Net(input_size, output_size)
         self.learning_target.transfer_weights(self.neural_net)
         self.memory = ReplayMemory(1000000)
-        self.batch_size = 10
-        self.update_time_steps = 3
+        self.batch_size = 128
+        self.update_time_steps = 10
         self.env_type = env_type
         self.input_size = input_size
         self.output_size = output_size
@@ -52,7 +52,7 @@ class Main:
         return action
     
     def main(self):
-        replay_memory_size = [1000000]
+        replay_memory_size = [10000]
 
         for size in replay_memory_size:
             self.neural_net = Net(self.input_size, self.output_size)
@@ -124,24 +124,26 @@ class Main:
     # here we are actually calcuating the y_pred and doing the backprop based on the loss.
     def train(self, current_states, actions, rewards, next_states,dones):
 
-        all_rewards = []
-        losses = []
-        # print('current_states 1 - ',len(current_states),current_states,next_states)
-        current_states = torch.stack(current_states, dim=0).to(device).float()
-        next_state_main = torch.stack(next_states, dim=0).to(device).float()
-        rewards = torch.stack(rewards,dim=0).to(device).float()
-        # print('current_states',current_states.shape,current_states,next_states)
-        target_next_state_predictions = self.target_network.forward(next_state_main)
-        nn_current_state_predictions = self.neural_net.forward(current_states)
-        # print("target_next_state_predictions",target_next_state_predictions.shape)
-        # print('rewards',rewards)
-        # print('target_next_state_predictions',target_next_state_predictions.shape)
-        # print('dones',dones)
-        dones = torch.stack(dones,dim=0).to(device).float()
-        # print('dones',dones)
-        # print('self.discount_factor' ,self.discount_factor )
-        all_rewards = rewards + self.discount_factor * torch.argmax(target_next_state_predictions, axis=1) * dones
-        
+        # all_rewards = []
+        # losses = []
+        # # print('current_states 1 - ',len(current_states),current_states,next_states)
+        # current_states = torch.stack(current_states, dim=0).to(device).float()
+        # next_state_main = torch.stack(next_states, dim=0).to(device).float()
+        # print('rewards: ',rewards)
+        # print('Dones: ',dones)
+        # rewards = torch.stack(rewards,dim=0).to(device).float()
+        # # print('current_states',current_states.shape,current_states,next_states)
+        # target_next_state_predictions = self.target_network.forward(next_state_main)
+        # nn_current_state_predictions = self.neural_net.forward(current_states)
+        # # print("target_next_state_predictions",target_next_state_predictions.shape)
+        # # print('rewards',rewards)
+        # # print('target_next_state_predictions',target_next_state_predictions.shape)
+        # # print('dones',dones)
+        # dones = torch.stack(dones,dim=0).to(device).float()
+        # # print('dones',dones)
+        # # print('self.discount_factor' ,self.discount_factor )
+        # # all_rewards = rewards + self.discount_factor * torch.argmax(target_next_state_predictions, axis=1) * dones
+        # # print(all_rewards)
         # for done, reward,target_next_state_prediction in zip(dones,rewards,target_next_state_predictions):
         #     if done:
         #         targetpred = reward.float()
@@ -151,26 +153,55 @@ class Main:
         #         q_value_pred = torch.max(target_next_state_prediction)
         #         targetpred = reward + self.discount_factor * q_value_pred
         #         all_rewards.append(targetpred)
-        criterion = nn.MSELoss()
-        new_rewards = []
-        for reward in all_rewards:
-            new_rewards.append(reward.to(device))
-        new_actions = []
-        for action in actions:
-            new_actions.append(action.to(device))
+        # print(all_rewards)
+        # criterion = nn.MSELoss()
+        # new_rewards = []
+        # for reward in all_rewards:
+        #     new_rewards.append(reward.to(device))
+        # new_actions = []
+        # for action in actions:
+        #     new_actions.append(action.to(device))
         
-        targetpred = torch.stack(new_rewards,dim=0)
-        targetpred = targetpred.to(device)
-        new_actions = torch.stack(new_actions,dim=0)
-        new_actions = new_actions.to(device)
-        temp_temp = torch.index_select(nn_current_state_predictions,1,new_actions)
-        # doing backprop and training the policy network.
-        loss = criterion(temp_temp,targetpred)
-        losses.append(loss)
+        # targetpred = torch.stack(new_rewards,dim=0)
+        # targetpred = targetpred.to(device)
+        # new_actions = torch.stack(new_actions,dim=0)
+        # new_actions = new_actions.to(device)
+        # temp_temp = torch.index_select(nn_current_state_predictions,1,new_actions)
+        # # doing backprop and training the policy network.
+        # loss = criterion(temp_temp,targetpred)
+        # losses.append(loss)
+        # self.neural_net.optimizer.zero_grad()
+        # loss.backward()
+        # # print(next(self.neural_net.parameters()).grad.data.clamp_(-1,1))
+        # # for param in self.neural_net.parameters():
+        #   # param.grad.data.clamp_(0, 100)
+        # self.neural_net.optimizer.step()
+        for index, s in enumerate(current_states):
+          current_states[index] = torch.tensor(s.float())
+        for index, s in enumerate(next_states):
+          next_states[index] = torch.tensor(s.float())
+        for index, s in enumerate(actions):
+          actions[index] = torch.tensor(s)
+        current_states = torch.stack(current_states,dim=0)
+        next_states = torch.stack(next_states, dim=0)
+        actions = torch.stack(actions, dim=0)
+        q_pred = self.neural_net.forward(current_states).gather(1, actions.view(-1,1))
+        target_q_values = self.target_network.forward(next_states).max(dim=1).values
+
+        y_target = list()
+        for index, value in enumerate(target_q_values):
+            if dones[index]:
+             y_target.append(rewards[index])
+            else:
+                y_target.append(rewards[index] + self.discount_factor * value)
+        
+        y_target = torch.stack(y_target,dim=0)
+        # print(y_target)
+        criterion = nn.MSELoss()
+        loss = criterion(y_target,q_pred)
+        # losses.append(loss)
         self.neural_net.optimizer.zero_grad()
         loss.backward()
-        # for param in self.neural_net.parameters():
-        #   param.grad.data.clamp_(-1, 1)
         self.neural_net.optimizer.step()
 
     # in this function, we are simply taking the greedy action and testing the policy learnt by the Neural network.
